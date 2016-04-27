9eb46b75504c5d4faf7e586667367f4f689436e9
MaartenBrijker <maartenbrijker@gmail.com>
2016-04-27 21:57:12 +0200

    works with coded list, but not yet with SQLite database

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -7,22 +7,84 @@
 //
 
 import UIKit
+import SQLite
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    let stuff = ["shopping", "blahblah", "moreblahblah"]
+class ViewController: UIViewController {
+    
+    var stuff = [String] ()
+    var newToDo = ""
+    
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addTextField: UITextField!
+
+    // Add todo to database
+    @IBAction func addButton(sender: AnyObject) {
+       
+        // add new 'to do' text
+        newToDo = addTextField.text!
+        stuff.append(newToDo)
+        self.tableView.reloadData()
+        
+        print(stuff)
+        
+    }
+    
+    // SQLite Database:
+    var database: Connection?
+    
+    let users = Table("users")
+    let id = Expression<Int64>("id")
+    let todo = Expression<String>("name")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup database
+        setupDatabase()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    private func setupDatabase() {
+        let path = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first!
+        
+        do {
+            // If connection is set up, create table.
+            database = try Connection("\(path)/db.sqlite3")
+            createTable()
+        }
+            
+        catch {
+            // Error handling here.
+            print("Cannot connect to database: \(error)")
+            
+        }
+    }
+    
+    private func createTable(){
+        
+        do {
+            try database!.run(users.create(ifNotExists: true) { t in    // CREATE TABLE: "users"
+            
+                t.column(id, primaryKey: .Autoincrement)            // "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                t.column(todo, unique: true)                        // "mail" TEXT UNIQUE NOT NULL,
+            })
+        }
+            
+        catch {
+            // Error handling here.
+            print("Failed to create table: \(error)")
+        }
+    }
+    
+    
+}
+    
+extension ViewController: UITableViewDataSource {
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return stuff.count
@@ -32,10 +94,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         let cell = self.tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! CustomCell
         
-        cell.toDoLabel.text = stuff[indexPath.row]
+        cell.toDoLabel.text = self.stuff[indexPath.row]
         
         return cell
         
     }
+}
+
+extension ViewController: UITableViewDelegate {
+    
 }
 
